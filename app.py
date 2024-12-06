bb06aaf598805e3c0e45b753e6f7844cd3e2e120
Ravi <ravikhichar9715@gmail.com>
2024-12-06 18:43:10 +0530

    libs and basic web_app creation

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import Flask, request, make_response
+import os, json
+from flask_cors import CORS,cross_origin
+
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello (RAVI), this is the Flask API!'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port =5000)
